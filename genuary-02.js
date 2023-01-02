01d2a2a7cc8d9b0430f6a9aa5a5d3020555c7795
MDN <mark.denardo@gmail.com>
2023-01-02 12:53:24 -0500

    Create genuary-02.js

---
+++ genuary-02.js
@@ -0,0 +1,16 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// #genuary2023 #genuary2 'make something in 10 minutes'
+// by MDN IG: @markdenardo
+const f = (()=>a.fft[0])
+const pi = (()=>Math.random(Math.PI))
+const osc1 =()=>{return osc(1,1,1)}
+shape(f,f,f)
+  .mult(voronoi(1000,2)
+  .blend(o0).luma())
+  .add(shape(3,0.125,f)
+       .rotate(1,1).mult(voronoi(1000,1).luma())
+       .rotate(1.5)).scrollX([0.1,-0.0625,0.005,0.00001],0)
+  .scrollY([0.1,-0.0625,0.005,0.00001].smooth(),0)
+.kaleid([0.5,0.25,0.125]).scale(0.5)
+  .out()
+render(o0)
