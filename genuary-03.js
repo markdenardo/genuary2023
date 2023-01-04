563d1d259673d28281fa39e935f01ab4fd724dd4
MDN <mark.denardo@gmail.com>
2023-01-03 21:02:36 -0500

    Create genuary-03.js

---
+++ genuary-03.js
@@ -0,0 +1,11 @@
+await loadScript('https://cdn.jsdelivr.net/npm/p5.glitch@latest/p5.glitch.js')
+s1.initVideo("https://media1.giphy.com/media/AcrHRET6oOqo1LTT0s/giphy.mp4")
+p1 = new P5()
+let 
+s0.init({src:p1.canvas})
+p1.draw=()=>{
+p1.background(0,0,0);
+	p1.rect()
+}
+src(s0).blend(s1).out(o0)
+render(o0)
