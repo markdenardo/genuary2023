5b12057e725cd184f45323d1fbe0e563e09312d2
MDN <mark.denardo@gmail.com>
2023-01-11 22:15:31 -0500

    Update genuary-10.rb

--- genuary-10.rb
+++ genuary-10.rb
@@ -6,13 +6,45 @@ people = '/Users/mdn/Desktop/jungle/calling-all-the-people.aif'
 
 load_sample people
 
-d = sample_duration people
-sl = 32
 
-live_loop :people do
-  sample people, slice: 6, num_slices: sl
-  sleep d/sl
+d = sample_duration people
+sl = 48
+with_fx :echo do
+  
+  live_loop :people do
+    
+    3.times do
+      sample people, slice: 9, num_slices: sl if one_in(2)
+      sleep d/sl
+      
+      sample people, slice: 7, num_slices: sl if one_in(1)
+      sleep d/sl
+      
+      sample people, slice: 1, num_slices: sl if one_in(3)
+      sleep d/sl
+      
+    end
+    
+    
+    8.times do
+      sample people, slice: 9, num_slices: sl if one_in(1)
+      sleep d/sl
+      
+    end
+    
+    2.times do
+      sample people, slice: 12, num_slices: sl
+      sleep d/sl
+      sample people, slice: 12, num_slices: sl
+      sleep d/sl
+    end
+    
+  end
+  
+  ##| live_loop :people1 do
+  ##|   sample people, slice: 19, num_slices: sl/2 if one_in(8)
+  ##|   sleep d/sl
+  ##| end
   
   
 end
-
